289e42d8c02943c4f7d2e3ca046be281f5180f1f
wim86 <wim-gits86@gmail.com>
2020-02-11 10:34:47 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
    Rails.application.routes.draw do
   get 'static_pages/home'
-
+  get  'static_pages/about'
   get 'static_pages/help'
-
-      root 'application#hello'
-   end
+  get 'static_pages/contact'
+  root 'static_pages#home'
+  
+  end
   
  
 end
