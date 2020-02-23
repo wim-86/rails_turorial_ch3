5684a3dd7c74a5c94f23b7833cddebe3c006d46b
masatug7@gmail.com <masatug7@gmail.com>
2020-02-23 20:50:38 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  root 'static_pages#home' # => root_path
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+
+  # => contact_path
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
-   Rails.application.routes.draw do
-  get 'static_pages/home'
-  get  'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  root 'static_pages#home'
-  
-  end
-  
- 
+  #root 'application#hello'
+
 end
